f9d24d2fae28eea2f0f258ef01d607192f91d02d
soprunveronika <71070202+soprunveronika@users.noreply.github.com>
2021-04-19 23:19:29 +0300

    update

--- src/main/scala/karazin/scala/users/group/week3/homework/program.scala
+++ src/main/scala/karazin/scala/users/group/week3/homework/program.scala
@@ -1,30 +1,27 @@
-/*
-
-Fix the code to make it compilable
-Write tests
-
 package karazin.scala.users.group.week3.homework
 
 import java.util.UUID
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutorService, Future}
 import scala.util.Success
 import scala.util.Failure
-
 import karazin.scala.users.group.week3.homework.model._
+import karazin.scala.users.group.week3.homework.services.{getComments, getLikes, getShares}
+
+import java.util.concurrent.Executors
+import scala.concurrent.ExecutionContext.Implicits.global
 
 
 object program:
 
-  def getPostView(post: Post): Future[PostView] = 
-    
-    val getCommentsService  = getComments(post.postId)
-    val getLikesService     = getLikes(post.postId)
-    val getSharesService    = getShares(post.postId)
+  def getPostView(post: Post): Future[PostView] =
+    val fixedThreadPoolContext: ExecutionContextExecutorService =
+        ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(3))
+    val getCommentsService  = getComments(post.postId)(fixedThreadPoolContext)
+    val getLikesService     = getLikes(post.postId)(fixedThreadPoolContext)
+    val getSharesService    = getShares(post.postId)(fixedThreadPoolContext)
     
     for
       comments  ← getCommentsService
       likes     ← getLikesService
       shares    ← getSharesService
     yield PostView(post, comments, likes, shares)
-    
- */
