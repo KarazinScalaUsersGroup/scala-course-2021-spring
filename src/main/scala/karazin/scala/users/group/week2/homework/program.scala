938d1bf1c684569ffb1fab97dfa0c1d2f7507708
soprunveronika <71070202+soprunveronika@users.noreply.github.com>
2021-04-19 23:26:07 +0300

    update week2

--- src/main/scala/karazin/scala/users/group/week2/homework/program.scala
+++ src/main/scala/karazin/scala/users/group/week2/homework/program.scala
@@ -10,24 +10,52 @@ object program:
   /*
    Print all view for all user's posts if they exists
   */
-  def printPostsViews(): ErrorOr[List[PostView]] = ???
- 
-  
+  def printPostsViews(): ErrorOr[List[PostView]] = 
+    for
+      postsViews ← getPostsViews()
+    yield
+      postsViews.foreach(postView => println(postView))
+      postsViews
+
   /*
    Getting view for all user's posts if they exists
   */
-  def getPostsViews(): ErrorOr[List[PostView]] = ???
+  def getPostsViews(): ErrorOr[List[PostView]] =
+    for
+      profile    ← getUserProfile()
+      posts      ← getPosts(profile.userId)
+      postsViews ← ErrorOr(posts map { post ⇒ getPostView(post) })
+      flattenPostsViews ← ErrorOr(postsViews.foldLeft(List[PostView]()){
+        (acc, element) ⇒ element match
+          case ErrorOr.Some(v: PostView) ⇒ v :: acc
+          case ErrorOr.Failer(_) ⇒ acc
+      })
+    yield flattenPostsViews
 
-  /* 
+  /*
     Getting view for a particular user's post
     Provide an argument and a result type
   */
-  def getPostView(post: Post) = ???
+  def getPostView(post: Post): ErrorOr[PostView] =
+    for
+      comments  ← getComments(post.postId)
+      likes     ← getLikes(post.postId)
+      shares    ← getShares(post.postId) 
+    yield PostView(post, comments, likes, shares)
 
-  /*
-   Provide desugared version of the previous two methods
-  */
-  def getPostsViewDesugared() = ???
+  def getPostsViewDesugared(): ErrorOr[List[ErrorOr[PostView]]] =
+    getUserProfile() flatMap { profile ⇒
+      getPosts(profile.userId)} map { posts ⇒
+      posts map { post ⇒
+        getComments(post.postId) flatMap { comments ⇒
+          getLikes(post.postId) flatMap { likes ⇒
+            getShares(post.postId) map { shares ⇒
+              PostView(post, comments, likes, shares)
+            }
+          }
+        }
+      }
+    }
 
 
   
